b0ea010c098cb666a17f3f96930de2e01a986932
Clem Davies <clem.wdavies@gmail.com>
2013-02-06 13:47:43 -0600

    validation of bookmark field lengths

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,9 @@
 class Bookmark < ActiveRecord::Base
   attr_accessible :date_saved, :name, :url
+
+
+  validates :name, :length => { :minimum => 5, :maximum => 50 }
+  validates :url, :length => { :minimum => 9, :maximum => 100 }
+
+
 end
